027234af174df34fb74b0a0ce765982c543288af
Joshua <mrjoshsimons@gmail.com>
2025-05-01 11:59:32 +1200

    Asking for important details

---
+++ v1.py
@@ -0,0 +1,10 @@
+#This is asking for the students name, age and school theym are coming from
+name = input("Please enter your name: ")
+age = input("Please enter your name: ")
+school = input("Please enter the school you are coming from: ")
+
+print("Can you please choose one of the 3 options")
+
+#Will show all the options
+print("Cultural Immersion, This goes for 5 days andis considered easy and costs $800")
+print("")
